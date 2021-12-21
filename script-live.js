66a73f2065e5ea8307552ea8101fc677aa0f467b
noamzuck <62641593+noamzuck@users.noreply.github.com>
2021-12-21 21:21:12 +0200

    Update script-live.js

--- script-live.js
+++ script-live.js
@@ -7,7 +7,7 @@ Promise.all([
     faceapi.nets.faceLandmark68Net.loadFromUri('/models1'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models1'),
     faceapi.nets.ssdMobilenetv1.loadFromUri('/models1')
-]).then(startVideo)
+]).then(startVideo(json))
 
 /*async function loadArr(json) {
     //let url = 'https://api.npoint.io/9efa4b6ac0f89af5b01c'
