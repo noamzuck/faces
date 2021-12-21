20ea9c357651023ef27f09fac4259c5281b8f7e5
noamzuck <62641593+noamzuck@users.noreply.github.com>
2021-12-21 19:32:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<?php 
-  /*$mydir = 'labeled_images'; 
-  
-  $myfiles = array_diff(scandir($mydir), array('.', '..')); 
-  
-  print_r($myfiles);*/
-    
-    
-$arr="[";
-foreach (new DirectoryIterator(__DIR__) as $file) {
-  if ($file->isFile()) {
-      $arr.="'".$file->getFilename()."'".", ";
-  }
-}
-$arr.="]";
-print_r($arr);
-?> 
+
