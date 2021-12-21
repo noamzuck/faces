53271eff234b5c08734ecdef03ab6c4bfbc49313
noamzuck <62641593+noamzuck@users.noreply.github.com>
2021-12-21 19:16:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
   $mydir = 'labeled_images'; 
   
-  $myfiles = array_diff(scandir($mydir), array('.', '..')); 
+  $myfiles = array_diff(scandir($mydir), array('..')); 
   
   print_r($myfiles); 
 ?>
